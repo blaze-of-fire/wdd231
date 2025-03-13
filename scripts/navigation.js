be045bab11912a97a043db6b255be0d8f0860d30
carterqarnold <carnold@byupathway.edu>
2025-03-12 22:38:52 -0600

    save changes just in case

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,5 @@
 const hambutton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
+const navigation = document.querySelector("#animateme");
 hambutton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     hambutton.classList.toggle("open");
