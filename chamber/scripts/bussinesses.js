6f2c99436bbae80e8030532cd0b727715d26afcc
carterqarnold <carnold@byupathway.edu>
2025-03-15 21:53:54 -0600

    update

--- chamber/scripts/bussinesses.js
+++ chamber/scripts/bussinesses.js
@@ -17,38 +17,47 @@ const displayBussinesses = (bussinesses) => {
         let phone_number = document.createElement("p");
         let address = document.createElement("p");
         let link = document.createElement("li");
+        let ul = document.createElement("ul")
 
         let website_url = document.createElement("a");
         let membership_level = document.createElement("p");
 
         name.setAttribute("class", "name");
 
-        image_url.setAttribute("src", bussiness.image_or_icon_url);
-        image_url.setAttribute("alt", `${bussiness.bussiness_name}`);
-
-        image_url.setAttribute("loading", "lazy");
-        image_url.setAttribute("width", "100");
+        if (bussiness.image_or_icon_url == ""){
+            image_url = null
+        }
         
-        image_url.setAttribute("height", "auto");
-        image_url.setAttribute("class", "logo");
-    
-        phone_number.textContent = `phone number: ${bussiness.phone_number}`;
-        phone_number.setAttribute("class", "phone_number");
+        image_url.setAttribute("src", `${bussiness.image_or_icon_url}`)
+        image_url.setAttribute("alt", `${bussiness.bussiness_name}`)
+        image_url.setAttribute("width", "100")
+        image_url.setAttribute("height", "auto")
 
-        address.textContent =   `Place of Birth: ${bussiness.address}`;
+
+    
+        phone_number.textContent = `${bussiness.phone_number}`;
+        phone_number.setAttribute("class", "phone_number");
+        phone_number.style.color = "black";
+
+        address.textContent =   `${bussiness.address}`;
         address.setAttribute("class", "address");
+        address.style.color = "black";
 
         website_url.setAttribute("href", bussiness.website_url);
-        website_url.textContent = `${bussiness.website_url}`
+        website_url.textContent = `Website: ${bussiness.webpage_nickname}`
+        website_url.style.color = "black";
+
+        website_url.style.fontSize = "medium";
+        website_url.style.marginLeft = "-1rem"
         membership_level.textContent = `${bussiness.membership_level}`;
         
 
         name.textContent = `${bussiness.bussiness_name}`;
-        
 
         link.append(website_url);
+        ul.append(link);
         card.setAttribute("class", "card");
-        card.append(image_url, address, phone_number, link, membership_level);
+        card.append(image_url, address, phone_number, ul, membership_level);
         cards.append(card);
     });
 };
