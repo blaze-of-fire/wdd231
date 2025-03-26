c5ce1074f957ecf9ad90bad534263918ada12cc8
carterqarnold <carnold@byupathway.edu>
2025-03-25 23:15:21 -0600

    the weather is working I just need to add style

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,4 +1,7 @@
 const weatherDiv = document.querySelector("#weather");
+const cWeatherDiv = document.querySelector("#current_weather");
+let weather = document.querySelector("#weather");
+
 
 const url = "https://api.openweathermap.org/data/2.5/forecast?lat=43.91&lon=-111.68&units=imperial&appid=2261e297cafa4fe59c9b011a4aebe694";
 
@@ -9,34 +12,61 @@ async function apiFetch() {
             throw Error(await response.text());
     } else {
         const data = await response.json();
-        console.log(data);
-        displayResults(data);
+        displayResults(data.list);
     }
     } catch (error) {
         console.log(error);
     }
 }
 
-function displayResults(data) {
-    data.forEach(data => {
-        console.log(data);
-        let cTempP = document.createElement('p');
-        let weatherIcon = document.createElement('img');
-        let currentTemp = document.createElement('span');
-        let weatherIconName = document.createElement('p');
-        let weatherSection = document.createElement('section');
-        cTempP.textContent = `The temerature is`;
-        currentTemp.textContent = `${data.main.temp}`;
-        const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-        let desc = data.weather[0].description;
-        weatherIcon.setAttribute("src", `${iconsrc}`);
-        weatherIcon.setAttribute("alt", "weatherIcon");
-        weatherIconName.textContent = `${desc}`;
-        cTempP.appendChild(currentTemp);
-        weatherSection.append(cTempP, weatherIcon, weatherIconName);
-        weatherDiv.append(weatherSection);
-    });
-}
+const time = new Date();
+let day = time.getDay();
+const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
 
+function displayResults(data) {
+    let count = 0;
+        data.forEach(object => {
+            if (count  < 4){
+            let dayH2 = document.createElement('p');
+            dayH2.textContent = `${daysOfWeek[(day+count)]}`;
+            let cTempP = document.createElement('p');
+            let weatherIcon = document.createElement('img');
+            let currentTemp = document.createElement('span');
+
+            let weatherIconName = document.createElement('p');
+            let weatherSection = document.createElement('section');
+            weatherSection.setAttribute("class", "weather_section")
+            cTempP.textContent = `The temerature is `;
+
+            currentTemp.textContent = `${object.main.temp}`;
+            const iconsrc = `https://openweathermap.org/img/w/${object.weather[0].icon}.png`;
+            let desc = object.weather[0].description;
+
+            weatherIcon.setAttribute("src", `${iconsrc}`);
+            weatherIcon.setAttribute("alt", "weatherIcon");
+            weatherIconName.textContent = `${desc}`;
+
+            cTempP.appendChild(currentTemp);
+            weatherSection.append(dayH2);
+            weatherSection.append(cTempP, weatherIcon, weatherIconName);
+            count = count + 1;
+            if (count != 1){
+                weather.append(weatherSection);
+            }
+            else{
+                cWeatherDiv.append(weatherSection);
+            }
+            }
+        });
+        weatherDiv.append(weather);
+};
 apiFetch();
 
