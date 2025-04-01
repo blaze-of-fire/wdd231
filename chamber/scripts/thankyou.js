5fcf003549572ec45b88f4f76f9257d4c2b3a24e
carterqarnold <carnold@byupathway.edu>
2025-04-01 15:46:57 -0600

    Thankyou page

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,8 @@
+const myInfo = new URLSearchParams(window.location.search);
+
+let results = document.querySelector('#results').innerHTML = `
+<h1 id="h1">Thank You!</h1>
+<p class="thank_ptags"><span class="the_results">${myInfo.get('fname')}</span> <span class="the_results">${myInfo.get('lname')}</span> as the <span class="the_results">${myInfo.get('organization-title')}</span> from <span class="the_results">${myInfo.get('organization')}</span> is applying for a <span class="the_results">${myInfo.get('choose_membership')}</span> membership</p>
+<p class="thank_ptags">Description: <span class="the_results">${myInfo.get('description')}</span></p>
+<p class="thank_ptags">Your Phone: <span class="the_results">${myInfo.get('phone')}</span></p>
+<p class="thank_ptags">Your email is: <span class="the_results">${myInfo.get('email')}</span></p>`;
