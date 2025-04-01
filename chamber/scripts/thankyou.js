9534bbbb99ae57d2a049f94c70dcbd49c976ab91
carterqarnold <carnold@byupathway.edu>
2025-04-01 15:49:21 -0600

    thankyou page

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -5,4 +5,4 @@ let results = document.querySelector('#results').innerHTML = `
 <p class="thank_ptags"><span class="the_results">${myInfo.get('fname')}</span> <span class="the_results">${myInfo.get('lname')}</span> as the <span class="the_results">${myInfo.get('organization-title')}</span> from <span class="the_results">${myInfo.get('organization')}</span> is applying for a <span class="the_results">${myInfo.get('choose_membership')}</span> membership</p>
 <p class="thank_ptags">Description: <span class="the_results">${myInfo.get('description')}</span></p>
 <p class="thank_ptags">Your Phone: <span class="the_results">${myInfo.get('phone')}</span></p>
-<p class="thank_ptags">Your email is: <span class="the_results">${myInfo.get('email')}</span></p>`;
+<p class="thank_ptags">Your Email: <span class="the_results">${myInfo.get('email')}</span></p>`;
