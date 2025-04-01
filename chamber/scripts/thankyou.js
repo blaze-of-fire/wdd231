6c42f3ac803a40f7001aa1531e83f0957e28d03f
carterqarnold <carnold@byupathway.edu>
2025-04-01 16:53:18 -0600

    done

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -2,7 +2,10 @@ const myInfo = new URLSearchParams(window.location.search);
 
 let results = document.querySelector('#results').innerHTML = `
 <h1 id="h1">Thank You!</h1>
-<p class="thank_ptags"><span class="the_results">${myInfo.get('fname')}</span> <span class="the_results">${myInfo.get('lname')}</span> as the <span class="the_results">${myInfo.get('organization-title')}</span> from <span class="the_results">${myInfo.get('organization')}</span> is applying for a <span class="the_results">${myInfo.get('choose_membership')}</span> membership</p>
-<p class="thank_ptags">Description: <span class="the_results">${myInfo.get('description')}</span></p>
+<p class="thank_ptags">Name: <span class="the_results">${myInfo.get('fname')}</span> <span class="the_results">${myInfo.get('lname')}</span></p>
+<p class="thank_ptags">Organization Title: <span class="the_results">${myInfo.get('organization-title')}</span></p>
+<p class="thank_ptags">Your Email: <span class="the_results">${myInfo.get('email')}</span></p>
 <p class="thank_ptags">Your Phone: <span class="the_results">${myInfo.get('phone')}</span></p>
-<p class="thank_ptags">Your Email: <span class="the_results">${myInfo.get('email')}</span></p>`;
+<p class="thank_ptags">Bussiness/Organization: <span class="the_results"> ${myInfo.get('organization')}</span></p>
+<p class="thank_ptags">Membership: <span class="the_results">${myInfo.get('choose_membership')}</span></p>
+<p class="thank_ptags">Description: <span class="the_results">${myInfo.get('description')}</span></p>`;
