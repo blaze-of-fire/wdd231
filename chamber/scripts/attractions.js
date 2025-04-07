56c4ebf7d03f70780bbb29ad28bb635507ca7c98
blaze-of-fire <carnold@byupathway.edu>
2025-04-07 12:49:03 -0600

    local storage

--- chamber/scripts/attractions.js
+++ chamber/scripts/attractions.js
@@ -1,3 +1,29 @@
+let currentTime = new Date();
+let currentDay = currentTime.getDay();
+const msg = document.getElementById("msg_using_timestamp");
+
+function timestampCheck(theTime, message) {
+    if(localStorage.getItem('timestamp') === null){
+        localStorage.setItem('timestamp', currentDay.toString());
+        message.textContent = "Welcome! Let us know if you have any questions.";
+        
+    }
+    else{
+        let storedTimestamp = localStorage.getItem('timestamp');
+
+        if (storedTimestamp === theTime.toString()){
+            message.textContent = "Back so soon! Awesome!";
+            localStorage.setItem('timestamp', theTime.toString());
+
+        }
+        else{
+        let daysAgo = (Number(storedTimestamp) - Number(theTime.toString())).toString();
+        message.textContent = `You last visited ${daysAgo} days ago.`;
+        localStorage.setItem('timestamp', theTime.toString());
+    }
+    }
+}
+
 const attractions = "./data/attractions.json";
 const sections_parent = document.getElementById('the_locations');
 
@@ -13,7 +39,7 @@ const displayAttractions = (locations) => {
         let card = document.createElement("section");
         let image = document.createElement("img");
         let title = document.createElement("h2");
-        let imageFigure = document.createElement("figure")
+        let imageFigure = document.createElement("figure");
         let address = document.createElement("address");
         let description = document.createElement("p");
         let button = document.createElement("button");
@@ -26,6 +52,7 @@ const displayAttractions = (locations) => {
         image.setAttribute("alt", `${attraction.name}`);
         image.setAttribute("width", "300");
         image.setAttribute("height", "auto");
+        image.setAttribute("class", "attraction_image");
 
         description.textContent = `${attraction.description}`;
         description.setAttribute("class", "attraction_desc");
@@ -45,4 +72,5 @@ const displayAttractions = (locations) => {
     });
 };
 
+timestampCheck(currentDay, msg);
 getAttractions();
