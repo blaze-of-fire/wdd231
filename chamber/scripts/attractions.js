9788394733b6905c51f9a12143471811a26d993e
blaze-of-fire <carnold@byupathway.edu>
2025-04-07 11:30:35 -0600

    added grid areas and quite a few other things

--- chamber/scripts/attractions.js
+++ chamber/scripts/attractions.js
@@ -5,7 +5,6 @@ async function getAttractions() {
     const response = await fetch(attractions);
     const data = await response.json();
     displayAttractions(data.attractions);
-    console.log(data.attractions);
 }
 
 const displayAttractions = (locations) => {
@@ -15,8 +14,7 @@ const displayAttractions = (locations) => {
         let image = document.createElement("img");
         let title = document.createElement("h2");
         let imageFigure = document.createElement("figure")
-        let addressPart1 = document.createElement("address");
-        let addressPart2 = document.createElement("address");
+        let address = document.createElement("address");
         let description = document.createElement("p");
         let button = document.createElement("button");
 
@@ -33,20 +31,16 @@ const displayAttractions = (locations) => {
         description.setAttribute("class", "attraction_desc");
         description.style.color = "black";
 
-        addressPart1.textContent = `${attraction.address_part1}`;
-        addressPart1.setAttribute("class", "attraction_address1");
-        addressPart1.style.color = "black";
-
-        addressPart2.textContent = `${attraction.address_part2}`;
-        addressPart2.setAttribute("class", "attraction_address2");
-        addressPart2.style.color = "black";
+        address.textContent = `${attraction.address}`;
+        address.setAttribute("class", "attraction_address");
+        address.style.color = "black";
         
         imageFigure.setAttribute("class", "image_figure");
         button.textContent = `Learn More!`;
         button.setAttribute("class", "learn_button");
 
         imageFigure.append(image);
-        card.append(title, imageFigure, addressPart1, addressPart2, description, button);
+        card.append(title, imageFigure, address, description, button);
         sections_parent.append(card);
     });
 };
