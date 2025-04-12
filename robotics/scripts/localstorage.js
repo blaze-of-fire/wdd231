0f5d5c849d7963232f3c9f2345d566e020d5ffda
blaze-of-fire <carnold@byupathway.edu>
2025-04-12 17:44:49 -0600

    update

---
+++ robotics/scripts/localstorage.js
@@ -0,0 +1,25 @@
+let currentTime = new Date();
+let currentDay = currentTime.getDay();
+const msg = document.getElementById("msg_using_timestamp");
+
+function timestampCheck(theTime, message) {
+    if(localStorage.getItem('timestamp') === null){
+        localStorage.setItem('timestamp', currentDay.toString());
+        message.textContent = "Welcome! Let us know if you have any questions.";
+        
+    }
+    else{
+        let storedTimestamp = localStorage.getItem('timestamp');
+
+        if (storedTimestamp === theTime.toString()){
+            message.textContent = "Back so soon! Awesome!";
+            localStorage.setItem('timestamp', theTime.toString());
+
+        }
+        else{
+        let daysAgo = (Number(storedTimestamp) - Number(theTime.toString())).toString();
+        message.textContent = `You last visited ${daysAgo} days ago.`;
+        localStorage.setItem('timestamp', theTime.toString());
+    }
+    }
+}
